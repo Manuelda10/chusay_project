aca1296b5fe36ccc5075714844242d340c564b13
Manuelda10 <manueldavidv.l@gmail.com>
2023-02-21 14:08:29 -0500

    Add carousel of screens

--- lib/pages/alert/principal_alert.dart
+++ lib/pages/alert/principal_alert.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:chusay_project/pages/alert/secondary_alert.dart';
 import 'package:flutter/material.dart';
 import 'package:chusay_project/pages/alert/first_alert.dart';
@@ -14,12 +15,66 @@ class AlertPrincipalScreen extends StatefulWidget {
 }
 
 class _AlertPrincipalScreen extends State<AlertPrincipalScreen>{
+  int _current = 0;
+  final CarouselController _controller = CarouselController();
+  final List<Widget> screensList = [
+    const FirstAlert(),
+    const SecondaryAlert(),
+    const ThirdAlert()
+  ];
+
+  // Realizar cambios en la 3era página para que entre en la pantalla
+  
   @override
   Widget build(BuildContext context){
-    return const Scaffold(
-      body: SingleChildScrollView(
+    return Scaffold(
+      /*body: SingleChildScrollView(
         child: ThirdAlert()
-      ),
+      ),*/
+      body: Column(
+        children: [
+          Builder(
+            builder: (context){
+              final double height = MediaQuery.of(context).size.height - 100;
+              return CarouselSlider(
+                carouselController: _controller,
+                items: screensList,
+                options: CarouselOptions(
+                  height: height,
+                  viewportFraction: 1,
+                  enlargeCenterPage: false,
+                  aspectRatio: 2.0,
+                  // autoPlay: false,
+                  onPageChanged: (index, reason) {
+                    setState(() {
+                      _current = index;
+                    });
+                  }
+                ),
+                );
+          }),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: screensList.asMap().entries.map((entry) {
+              return GestureDetector(
+                onTap: () => _controller.animateToPage(entry.key),
+                child: Container(
+                  width: 12.0,
+                  height: 12.0,
+                  margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: (Theme.of(context).brightness == Brightness.dark
+                            ? Colors.white
+                            : Colors.black)
+                        .withOpacity(_current == entry.key ? 0.9 : 0.4)),
+                ),
+              );
+            }).toList(),
+          ),  
+        ],
+      )
+      
     );
   }   
 }
